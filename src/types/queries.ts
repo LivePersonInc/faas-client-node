0c424141aae3e6b3e605338ae46997bd9417c20f
eplaza <eplaza@liveperson.com>
2025-03-07 16:41:17 +0100

    deprecate V1 interface

--- src/types/queries.ts
+++ src/types/queries.ts
@@ -1,8 +1,14 @@
+/**
+ * @deprecated Not available in Functions V2
+ */
 export interface BaseQuery {
   readonly v: string;
   readonly externalSystem: string;
   readonly skillId?: string;
 }
+/**
+ * @deprecated will be replaced GetFunctionsQuery in Functions V2
+ */
 export interface GetLambdasQuery extends Partial<BaseQuery> {
   readonly eventId?: string;
   readonly state?: string | string[];
